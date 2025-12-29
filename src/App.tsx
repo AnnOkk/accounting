bce084d3b4c0c2e1d62ccfb930d7d13f06c4fff6
AnnOkk <chereshnyasladkaya@gmail.com>
2025-12-29 18:00:35 +0200

    Dz ProtectedRoute mirror

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import {Route, Routes} from "react-router-dom";
 import Guest from "./components/Guest";
 import ProtectedRouting from "./components/Profile/ProtectedRouting.tsx";
 import GuestOnly from "./components/Guest/GuestOnly.tsx";
-import SignIn from "./components/Guest/SignIn.tsx";
-import SignUp from "./components/Guest/SignUp.tsx";
+
 
 
 function App() {
@@ -21,17 +20,14 @@ function App() {
             {/*Guest pages*/}
             <Route element={<GuestOnly isAuthentificated={isAuthenticated}/>}>
                 <Route path='/' element={<Guest/>}/>
-                <Route path='/signin' element={<SignIn/>}/>
-                <Route path='/signup' element={<SignUp/>}/>
+
             </Route>
 
 
                 {/*Protected*/}
             <Route element={<ProtectedRouting isAuthentificated={isAuthenticated}/>}>
                 <Route path='/profile' element={<Profile/>}/>
-                {/*<Route path='/profile/updateuser' element={<UpdateUser/>}/>*/}
-                {/*<Route path='/profile/editprofile' element={<EditProfile/>}/>*/}
-                {/*<Route path='/profile/changepassword' element={<ChangePassword/>}/> useAuth +  useNavigate because of close() */}
+
             </Route>
         </Routes>
 
